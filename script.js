c572e12d667a7cb7efd13971b96cc6e860fd3282
Arjun <arjunsemwal02@gmail.com>
2024-11-20 21:34:46 +0530

    updated the script with layout

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ const initialQuestions = [
 const storedQuestions = JSON.parse(localStorage.getItem('storedQuestions'));
 
 const questions = initialQuestions || storedQuestions;
+
+const makeStars = (maxValue, question) => {
+    const starContainer = document.createElement('div');
+
+    for( let starPosition = 1; starPosition < maxValue; starPosition++){
+        const starElement = document.createElement('span');
+        starContainer.appendChild(starElement);
+
+        if(starPosition <= question.rating)     
+            starElement.classList.add('filled');
+        else    
+            starElement.classList.add('empty');
+
+        return starContainer;
+    }
+}
+
+const makeStarRating = question => {
+    const container = document.createElement('div');
+    const label = document.createElement('label');
+    label.textContent = question.label;
+    container.appendChild(label);
+    container.appendChild(makeStars(5, question));
+
+    return container;
+}
+
+const ratingElement = document.getElementsByClassName('ratings');
+
+questions.forEach(question => {
+    ratingElement.appendChild(makeStarRating(question));
+})
